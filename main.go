9cb52cb3d422dbb86d67e58c8bb8da4a150bc922
Michael Van Veen <michael@mvanveen.net>
2015-04-02 20:36:50 -0700

    accept packets!

--- main.go
+++ main.go
@@ -7,28 +7,50 @@ import (
         "strings"
         "strconv"
 
-	"github.com/kaey/framebuffer"
+	"github.com/mvanveen/framebuffer"
 )
 
+type message_from struct {
+    message string
+    ip string
+    conn *net.UDPConn
+}
 
-func handleMessage(fb *framebuffer.Framebuffer, msg string) {
-   parts := strings.SplitN(msg, " ", 5)
+func handleMessage(fb *framebuffer.Framebuffer, msg string, ip string, conn *net.UDPConn) {
+   parts := strings.SplitN(msg, " ", 6)
 
-   x_coord, _ := strconv.Atoi(parts[0])
-   y_coord, _ := strconv.Atoi(parts[1])
+   cmd := parts[0]
 
-   red , _ := strconv.Atoi(parts[2])
-   blue , _ := strconv.Atoi(parts[3])
-   green , _ := strconv.Atoi(parts[4])
+   x_coord, _ := strconv.Atoi(parts[1])
+   y_coord, _ := strconv.Atoi(parts[2])
 
-   SendColor(fb, x_coord, y_coord, red, blue, green)
+   if cmd == "get" {
+      //sendResponse()
+      host := net.UDPAddr{
+          Port: 6668,
+          IP: net.ParseIP(ip),
+      }
 
-   // send message to channel to incrememnt color
-   fmt.Println("got message: (", x_coord, ", ", y_coord, ") ", red, blue, green)
+      color := fb.GetPixel(x_coord, y_coord)
+      response := fmt.Sprintf("%d %d %d %d %d", x_coord, y_coord, color[0], color[1], color[2])
+
+      conn.WriteToUDP([]byte(response), &host)
+
+   } else {
+
+      x_coord, _ := strconv.Atoi(parts[0])
+      y_coord, _ := strconv.Atoi(parts[1])
+
+      red, _ := strconv.Atoi(parts[2])
+      green, _ := strconv.Atoi(parts[3])
+      blue, _ := strconv.Atoi(parts[4])
+
+      SendColor(fb, x_coord, y_coord, red, blue, green)
+   }
 }
 
 
-func myUDPServer(messages chan string) {
+func myUDPServer(messages chan message_from) {
     fmt.Println("opening UDP server")
 
     LISTENING_IP := "0.0.0.0"
@@ -38,6 +60,7 @@ func myUDPServer(messages chan string) {
         Port: LISTENING_PORT,
         IP: net.ParseIP(LISTENING_IP),
     }
+
     conn, err := net.ListenUDP("udp", &addr)
     defer conn.Close()
     if err != nil {
@@ -48,7 +71,6 @@ func myUDPServer(messages chan string) {
 
     for {
         rlen, address, err := conn.ReadFromUDP(buf)
-
         if err != nil {
             fmt.Println("error reading data from connection")
             fmt.Println(err)
@@ -57,7 +79,7 @@ func myUDPServer(messages chan string) {
 
         if address != nil {
             if rlen > 0 {
-                messages <- string(buf[0:rlen])
+                messages <- message_from{string(buf[0:rlen]), address.String(), conn}
             }
         }
      }
@@ -67,23 +89,23 @@ func SendColor(fb *framebuffer.Framebuffer, x_coord int, y_coord int, red int, g
     fb.WritePixel(x_coord, y_coord, red, green, blue, 255)
 }
 
-func handleScreen(fb *framebuffer.Framebuffer, messages chan string) {
+func handleScreen(fb *framebuffer.Framebuffer, messages chan message_from) {
     fb.Clear(0, 0, 0, 0)
     for {
         msg := <- messages
-        handleMessage(fb, msg)
+        handleMessage(fb, msg.message, msg.ip, msg.conn)
     }
 }
+
 
 func main() {
 	fb, err := framebuffer.Init("/dev/fb0")
 	if err != nil {
-		log.Fatalln(err)
+                log.Fatalln(err)
 	}
 	defer fb.Close()
+        messages := make(chan message_from, 10000)
 
-        messages := make(chan string, 10000)
-
-        go myUDPServer(messages)
-        handleScreen(fb, messages)
+        go handleScreen(fb, messages)
+        myUDPServer(messages)
 }
